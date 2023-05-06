806f6284100a39271770beb6f0f2998bb00bc5a2
ui-d <dawiddeveloper@gmail.com>
2023-05-06 20:55:24 +0200

    fix: prettier formating fixed

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -3,19 +3,14 @@ import { useRouter } from 'next/router'
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-    title: 'Next.js + Tailwind CSS + TypeScript Starter',
-    siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
-    description:
-        'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-    /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-    url: 'https://tsnext-tw.thcl.dev',
+    title: 'Veed.io Challenge',
+    siteName: 'Veed.io Challenge',
+    description: 'Veed.io Challenge',
+    url: '/',
     type: 'website',
     robots: 'follow, index',
-    /**
-     * No need to be filled, will be populated with openGraph function
-     * If you wish to use a normal image, just specify the path below
-     */
-    image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
+
+    image: '',
 }
 
 type SeoProps = {
